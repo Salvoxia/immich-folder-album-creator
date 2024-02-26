6e03f209efee37ee2193bb78e69bc22dd2b81fdc
Salvoxia <salvoxia@blindfish.info>
2024-02-26 21:59:17 +0100

    Docker Reset crontab file before adding the cron expression to prevent multiplying the same entry on every container restart Fixes #1

--- docker/setup_cron.sh
+++ docker/setup_cron.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env sh
 
 if [ ! -z "$CRON_EXPRESSION" ]; then
-    (crontab -l 2>/dev/null; echo "$CRON_EXPRESSION /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2") | crontab -
+    CRONTAB="$CRON_EXPRESSION /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2"
+    # Reset crontab
+    crontab -r
+    (crontab -l 2>/dev/null; echo "$CRONTAB") | crontab -
+
     # Make environment variables accessible to cron
     printenv > /etc/environment
 fi
