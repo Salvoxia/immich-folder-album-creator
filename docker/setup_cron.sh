06ccbc728c8e85f2e250309f753db5d7dbb8e5e9
Salvoxia <Salvoxia@blindfish.info>
2024-08-02 19:35:31 +0200

    Cron Call the script with env variable UNATTENDED set to 1

--- docker/setup_cron.sh
+++ docker/setup_cron.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 if [ ! -z "$CRON_EXPRESSION" ]; then
-    CRONTAB="$CRON_EXPRESSION /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2"
+    CRONTAB="$CRON_EXPRESSION UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2"
     # Reset crontab
     crontab -r
     (crontab -l 2>/dev/null; echo "$CRONTAB") | crontab -
