ea4648b5a3baa95dba5749aae3a001b287b22831
Salvoxia <Salvoxia@blindfish.info>
2025-11-16 16:54:17 +0100

    feat: Run container rootless with supercronic

--- docker/setup_cron.sh
+++ docker/setup_cron.sh
@@ -1,15 +1,10 @@
 #!/usr/bin/env sh
-if [ ! -z "$RUN_IMMEDIATELY" ]; then
-    if [ "$RUN_IMMEDIATELY" = "true" ] || [ "$RUN_IMMEDIATELY" = "1" ]; then
-        UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2 || true
-    fi
-fi
 if [ ! -z "$CRON_EXPRESSION" ]; then
-    CRONTAB="$CRON_EXPRESSION UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2"
-    # Reset crontab
-    crontab -r
-    (crontab -l 2>/dev/null; echo "$CRONTAB") | crontab -
-
-    # Make environment variables accessible to cron
-    printenv > /etc/environment
+    echo "$CRON_EXPRESSION UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2" > $CRONTAB_PATH
+    if [ "$LOG_LEVEL" == "DEBUG" ]; then
+        $DEBUG_PARM=-debug
+    fi
+    /usr/local/bin/supercronic $DEBUGPARM $CRONTAB_PATH
+else
+    UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2 || true
 fi
