9ff6bbeca17c9f95b327903cfae3845b0437f220
Salvoxia <Salvoxia@blindfish.info>
2025-11-22 22:19:02 +0100

    fix: Docker: Enable supercronic debug logging when LOG_LEVEL == DEBUG

--- docker/setup_cron.sh
+++ docker/setup_cron.sh
@@ -7,9 +7,9 @@ if [ ! -z "$CRON_EXPRESSION" ]; then
     # populate crontab
     echo "$CRON_EXPRESSION UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2" > "$CRONTAB_PATH"
     if [ "$LOG_LEVEL" == "DEBUG" ]; then
-        $DEBUG_PARM=-debug
+        DEBUG_PARM=-debug
     fi
-    /usr/local/bin/supercronic -passthrough-logs -no-reap -split-logs $DEBUGPARM $CRONTAB_PATH
+    /usr/local/bin/supercronic -passthrough-logs -no-reap -split-logs $DEBUG_PARM $CRONTAB_PATH
 else
     UNATTENDED=1 /script/immich_auto_album.sh > /proc/1/fd/1 2>/proc/1/fd/2 || true
 fi
