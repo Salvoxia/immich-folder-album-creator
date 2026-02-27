a4ad868d9f1c601a095dbe4db670cdd75e30550d
Zelong <54531807+Dragon-GCS@users.noreply.github.com>
2026-02-27 09:43:30 +0800

    Elegant Interruption
    Add keyboard interrupt handling for abort album creation

--- immich_auto_album.py
+++ immich_auto_album.py
@@ -2096,9 +2096,11 @@ class FolderAlbumCreator():
             if is_docker:
                 print("Check that this is the list of albums you want to create. Run the container with environment variable UNATTENDED set to 1 to actually create these albums.")
                 return
-            else:
-                print("Press enter to create these albums, Ctrl+C to abort")
+            try:
                 input()
+            except KeyboardInterrupt:
+                print("Aborting...")
+                return
 
         logging.info("Listing existing albums on immich")
 
